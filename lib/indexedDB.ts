7227512c0040b11b744b9dfaf3613c7df3eb002c
Rueskas <Rueskas@hotmail.com>
2024-11-14 12:19:19 +0100

    Comprobar si existe geojson

--- lib/indexedDB.ts
+++ lib/indexedDB.ts
@@ -26,6 +26,16 @@ class GeoJsonDatabase extends Dexie {
 
 const db = new GeoJsonDatabase();
 
+async function fileExists(url: string): Promise<boolean> {
+  try {
+      const response = await fetch(url, { method: "HEAD" });
+      return response.ok;
+  } catch (error) {
+      console.error("Error checking file existence:", error);
+      return false;
+  }
+}
+
 export async function fetchAndSaveGeoJson(id: string) {
   try {
     const savedCarreterasGeoJson = await db.carreteras.get(`carreteras_${id}`);
@@ -40,15 +50,33 @@ export async function fetchAndSaveGeoJson(id: string) {
 
     const localizacion = localizaciones.find((obj) => obj.value === id);
     
-    const geojsonCarreterasData = await fetch(localizacion!.geojson_carreteras).then(
-      (response) => response.json()
-    );
-    const geojsonGarajesData = await fetch(localizacion!.geojson_garajes).then(
-      (response) => response.json()
-    );
+    const existeCarreteras = await fileExists(localizacion!.geojson_carreteras);
 
-    await db.carreteras.put({ id: `carreteras_${id}`, data: geojsonCarreterasData });
-    await db.garajes.put({ id: `garajes_${id}`, data: geojsonGarajesData });
+
+    let geojsonCarreterasData = {
+      type: "FeatureCollection",
+      features: []
+    }
+    if(existeCarreteras){
+      geojsonCarreterasData = await fetch(localizacion!.geojson_carreteras).then(
+        (response) => response.json()
+      );
+      await db.carreteras.put({ id: `carreteras_${id}`, data: geojsonCarreterasData });
+    }
+
+    const existeGarajes = await fileExists(localizacion!.geojson_garajes);
+    
+    let geojsonGarajesData = {
+      type: "FeatureCollection",
+      features: []
+    }
+    if(existeGarajes){
+      geojsonGarajesData = await fetch(localizacion!.geojson_garajes).then(
+        (response) => response.json()
+      );
+      await db.garajes.put({ id: `garajes_${id}`, data: geojsonGarajesData });
+    }
+
     return { carreteras: geojsonCarreterasData, garajes: geojsonGarajesData };
     
 
